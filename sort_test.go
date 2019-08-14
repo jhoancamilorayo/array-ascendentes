fa44a880d159ed3cda49e283eb641accc95ab46b
jhoancamilorayo <53912963+jhoancamilorayo@users.noreply.github.com>
2019-08-14 11:16:14 -0500

    Update sort_test.go

--- sort_test.go
+++ sort_test.go
@@ -1,1 +1,18 @@
+package Test
+
+import ("testing"
+       "github.com/stretchr/testify/require")
+
+func TestSort(t *testing.T){
+  c:= require.New(t)
+  
+  unsorted := []int{4,1,2,5,6,7}
+  
+  sorted := sortSlice(unsorted)
+  
+  expect := []int{1,2,4,5,6,7}
+  
+  c.Equal(expected, sorted)
+}
+
 
